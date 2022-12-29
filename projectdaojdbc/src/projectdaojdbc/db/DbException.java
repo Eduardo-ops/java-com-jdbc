a8a8c438b1fc53f7f36796aa4c090294012b83d6
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2022-12-29 11:09:44 -0300

    Criacao e inicio do projeto

---
+++ projectdaojdbc/src/projectdaojdbc/db/DbException.java
@@ -0,0 +1,14 @@
+package projectdaojdbc.db;
+
+/**
+ * Exception custom
+ */
+public class DbException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DbException(String msg) {
+		super(msg);
+	}
+
+}
