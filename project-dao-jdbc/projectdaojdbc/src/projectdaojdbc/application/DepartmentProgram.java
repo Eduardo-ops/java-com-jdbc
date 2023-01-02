e86d5f2a233cc00f055146e8b93bf3868127f1aa
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2023-01-02 09:33:56 -0300

    Comentario removidos

--- project-dao-jdbc/projectdaojdbc/src/projectdaojdbc/application/DepartmentProgram.java
+++ project-dao-jdbc/projectdaojdbc/src/projectdaojdbc/application/DepartmentProgram.java
@@ -14,21 +14,21 @@ public class DepartmentProgram {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		Scanner scn = new Scanner(System.in);
 
-//		System.out.println("********* Test1 Insert **********");
-//		Department departmentTest1 = new Department(null, "TESTE DELETE");
-//		departmentDao.insert(departmentTest1);
+		System.out.println("********* Test1 Insert **********");
+		Department departmentTest1 = new Department(null, "TESTE DELETE");
+		departmentDao.insert(departmentTest1);
 
-//		System.out.println("********* Test2 Update **********");
-//		Department departmentTest2 = departmentDao.findById(6);
-//		departmentTest2.setName("COMMERCIAL");
-//		departmentDao.update(departmentTest2);
+		System.out.println("********* Test2 Update **********");
+		Department departmentTest2 = departmentDao.findById(6);
+		departmentTest2.setName("COMMERCIAL");
+		departmentDao.update(departmentTest2);
 
-//		System.out.println("********* Test3 Delete **********");
-//		departmentDao.deleteById(9);
+		System.out.println("********* Test3 Delete **********");
+		departmentDao.deleteById(9);
 
-//		System.out.println("********* Test4 findById **********");
-//		Department departmentTest4 = departmentDao.findById(4);
-//		System.out.println(departmentTest4);
+		System.out.println("********* Test4 findById **********");
+		Department departmentTest4 = departmentDao.findById(4);
+		System.out.println(departmentTest4);
 
 		System.out.println("********* Tes5 findAll **********");
 		List<Department> listDepartmentTest5 = departmentDao.findAll();
